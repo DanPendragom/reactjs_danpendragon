0d0b2ad1aa247cdc648494be7fde360f34572eca
Daniel Pendragon <carlos.coutinho5@etec.sp.gov.br>
2020-03-10 04:26:42 -0300

    Updates

---
+++ precache-manifest.3c5fd3a8beb2a53da2091bd11ba204c0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3fc7e6136949ce8e5f3a636ab1fc54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb241927b59b5b6a83c1",
+    "url": "/static/css/main.001a2e0e.chunk.css"
+  },
+  {
+    "revision": "c50d3683d047689759ba",
+    "url": "/static/js/2.26165728.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.26165728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb241927b59b5b6a83c1",
+    "url": "/static/js/main.e5e9c946.chunk.js"
+  },
+  {
+    "revision": "4daafe7fb47304948735",
+    "url": "/static/js/runtime-main.16f87117.js"
+  },
+  {
+    "revision": "64a4009279239d381f2b23101abb2a10",
+    "url": "/static/media/ProductSans-Bold.64a40092.woff"
+  },
+  {
+    "revision": "eb964ca850eecca0ccad41e2905742cc",
+    "url": "/static/media/ProductSans-Bold.eb964ca8.woff2"
+  },
+  {
+    "revision": "c8293b1aa94dbf7d293d578542a5855c",
+    "url": "/static/media/ProductSans-BoldItalic.c8293b1a.woff"
+  },
+  {
+    "revision": "d9c307a828d22fa8a0af43b32b95070c",
+    "url": "/static/media/ProductSans-BoldItalic.d9c307a8.woff2"
+  },
+  {
+    "revision": "29c275d0c323dfba28caaeb26215c5fb",
+    "url": "/static/media/ProductSans-Italic.29c275d0.woff2"
+  },
+  {
+    "revision": "961630e8be3c2fa098d1e589925ef59f",
+    "url": "/static/media/ProductSans-Italic.961630e8.woff"
+  },
+  {
+    "revision": "a2a67cca7e731eecd64d7689f6e58360",
+    "url": "/static/media/ProductSans-Regular.a2a67cca.woff2"
+  },
+  {
+    "revision": "d3e70bb9f3e0c92a66905d70ba60d740",
+    "url": "/static/media/ProductSans-Regular.d3e70bb9.woff"
+  },
+  {
+    "revision": "ce32c5dbd51d117fe54e77153cc9f79a",
+    "url": "/static/media/daniel-pendragon.ce32c5db.svg"
+  },
+  {
+    "revision": "fc7a0ea44751dbb25fa69c92001e15fb",
+    "url": "/static/media/homepage-wall-background.fc7a0ea4.png"
+  },
+  {
+    "revision": "73b1b06bee6b97a991045f3771760b33",
+    "url": "/static/media/neon-blue.73b1b06b.svg"
+  },
+  {
+    "revision": "0a22b499af73d899f225c922d1e0733a",
+    "url": "/static/media/neon-webfont.0a22b499.woff"
+  },
+  {
+    "revision": "cb5e6beb1ec573b5dba562e2bc3c7588",
+    "url": "/static/media/neon-webfont.cb5e6beb.woff2"
+  },
+  {
+    "revision": "15bf3aca40023c2c62d5b52f604009bb",
+    "url": "/static/media/neon-yellow.15bf3aca.svg"
+  }
+]);
